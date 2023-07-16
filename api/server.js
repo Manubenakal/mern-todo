4e3458a90eac481e39d2fc6e1d1a826db2b0b262
Manu M Benakal <manubenakal@gmail.com>
2023-07-16 15:44:32 +0530

    Improved responsiveness

--- api/server.js
+++ api/server.js
@@ -7,54 +7,60 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
-	useNewUrlParser: true, 
-	useUnifiedTopology: true 
-}).then(() => console.log("Connected to MongoDB")).catch(console.error);
+mongoose
+  .connect(
+    'mongodb+srv://manubenakal:Kingceaser7@cluster0.nknhnbl.mongodb.net/?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(console.error);
 
 // Models
 const Todo = require('./models/Todo');
 
 app.get('/todos', async (req, res) => {
-	const todos = await Todo.find();
+  const todos = await Todo.find();
 
-	res.json(todos);
+  res.json(todos);
 });
 
 app.post('/todo/new', (req, res) => {
-	const todo = new Todo({
-		text: req.body.text
-	})
+  const todo = new Todo({
+    text: req.body.text,
+  });
 
-	todo.save();
+  todo.save();
 
-	res.json(todo);
+  res.json(todo);
 });
 
 app.delete('/todo/delete/:id', async (req, res) => {
-	const result = await Todo.findByIdAndDelete(req.params.id);
+  const result = await Todo.findByIdAndDelete(req.params.id);
 
-	res.json({result});
+  res.json({ result });
 });
 
 app.get('/todo/complete/:id', async (req, res) => {
-	const todo = await Todo.findById(req.params.id);
+  const todo = await Todo.findById(req.params.id);
 
-	todo.complete = !todo.complete;
+  todo.complete = !todo.complete;
 
-	todo.save();
+  todo.save();
 
-	res.json(todo);
-})
+  res.json(todo);
+});
 
 app.put('/todo/update/:id', async (req, res) => {
-	const todo = await Todo.findById(req.params.id);
+  const todo = await Todo.findById(req.params.id);
 
-	todo.text = req.body.text;
+  todo.text = req.body.text;
 
-	todo.save();
+  todo.save();
 
-	res.json(todo);
+  res.json(todo);
 });
 
 app.listen(3001);
